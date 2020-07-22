a1ca7bf87530e87d5554fd25c8eef45403fb6441
DumanTorbayev <dumanchikt@gmail.com>
2020-07-23 00:47:59 +0600

    add .env

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const APIkey = '19e8266c255d5d99c5c0bd92f36997a3'
+const APIkey = process.env.REACT_APP_API_KEY
 
 export const getData = (searchValue) => {
     const weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&APPID=${APIkey}&units=metric`;
