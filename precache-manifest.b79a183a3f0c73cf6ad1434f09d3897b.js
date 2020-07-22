76f0ac80a5a76317b921f4577dd59af81bd909ef
DumanTorbayev <dumanchikt@gmail.com>
2020-07-22 22:51:32 +0600

    Updates

---
+++ precache-manifest.b79a183a3f0c73cf6ad1434f09d3897b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982fef5692d89a644915eed1d3ca15f3",
+    "url": "/DumanTorbayev/weather-react-app/index.html"
+  },
+  {
+    "revision": "8c38bdd6ea823194bf2b",
+    "url": "/DumanTorbayev/weather-react-app/static/css/main.9f9fadbb.chunk.css"
+  },
+  {
+    "revision": "90249656bcc90832ba36",
+    "url": "/DumanTorbayev/weather-react-app/static/js/2.cf0539fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DumanTorbayev/weather-react-app/static/js/2.cf0539fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c38bdd6ea823194bf2b",
+    "url": "/DumanTorbayev/weather-react-app/static/js/main.68c7595e.chunk.js"
+  },
+  {
+    "revision": "fd0844eb2e027522e6f1",
+    "url": "/DumanTorbayev/weather-react-app/static/js/runtime-main.3a90f2ac.js"
+  },
+  {
+    "revision": "fc2459a9896d7cf5c9875612350e0ef9",
+    "url": "/DumanTorbayev/weather-react-app/static/media/bg.fc2459a9.jpg"
+  }
+]);
